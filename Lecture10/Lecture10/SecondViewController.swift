59cd2b7de46bd1b479f14b4ac16a46db941b3348
Van Simmons <rvsrvs@gmail.com>
2017-11-14 09:44:01 -0500

    As taught Lecture 10

---
+++ Lecture10/Lecture10/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  Lecture10
+//
+//  Created by Van Simmons on 11/13/17.
+//  Copyright © 2017 Harvard University. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var step: UIStepper!
+    @IBOutlet weak var grayView: UIView!
+    @IBOutlet weak var tealView: UIView!
+    @IBOutlet weak var fusciaView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func step(_ sender: UIStepper) {
+        let step = sender.value
+        var bounds = grayView.bounds
+        bounds.origin.y += CGFloat(step * 10.0)
+        grayView.bounds = bounds
+    }
+    
+}
+
